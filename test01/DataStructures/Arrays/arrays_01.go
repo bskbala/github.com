34fc8afae091cf8c26a1be09d78e57c400dce6f0
bskbala <bskbala410@gmail.com>
2021-12-13 22:01:06 -0600

    Arrays

--- test01/DataStructures/Arrays/arrays_01.go
+++ test01/DataStructures/Arrays/arrays_01.go
@@ -5,18 +5,18 @@ import (
 )
 
 func main() {
-	var x [256]int
+	var x [256]string
 	fmt.Println(len(x))
 
 	fmt.Println(x[42])
 	for i := 0; i < 256; i++ {
-		x[i] = 1
+		x[i] = string(i)
 	}
 
-	for i, v := range x {
-		fmt.Println("%v -%T - %b\n", v, v, v)
-		if i > 50 {
-			break
-		}
+	for _, v := range x {
+		fmt.Println("%v -%T - %v\n", v, v, []byte(v))
+		// if i > 50 {
+		// 	break
+		// }
 	}
 }
